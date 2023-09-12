12cdf2d78627292a25154cad02f057963410402d
Christine Cion <cion.c.bscs@gmail.com>
2023-09-13 06:55:56 +0800

    added middleware and error handling

--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
+// Add a built-in middleware
+app.use(express.json());
 app.use("/api/contacts",require("./routes/contactRoutes"));
+// added error handler
+app.use(errorHandler);
+
 
 app.listen(port, () => {
     console.log('Server running on port ${port}');
